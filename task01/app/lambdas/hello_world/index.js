4cb55b237be46f6d33fc7e3dbc52c1e9a0fcc974
yktejCantire <ykrishnateja20@gmail.com>
2024-04-27 16:51:55 +0530

    lambda func using syndicate

---
+++ task01/app/lambdas/hello_world/index.js
@@ -0,0 +1,9 @@
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        //body: JSON.stringify('Hello from Lambda!'),
+        message: "Hello from Lambda!",
+    };
+    return response;
+};
